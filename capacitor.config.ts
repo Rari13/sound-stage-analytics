83efce0948f4526109c113fdf7d0333510b3a35d
Rari13 <174602007+Rari13@users.noreply.github.com>
2026-01-22 11:00:24 -0500

    fix: remove Lovable server URL to load local files and fix redirection

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,6 @@ const config: CapacitorConfig = {
   appName: 'Spark Events',
   webDir: 'dist',
   server: {
-    // Hot-reload activé pour le développement
-    url: 'https://f6d30a56-2b5b-47c5-8e2c-a28853d84376.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
     androidScheme: 'https'
   },
   plugins: {
